fa94e3ebea8c71ad58fdcd1f415653ed629d2564
alfianyusufabdullah <alfianyabdullah@gmail.com>
2022-07-24 14:27:01 +0700

    initial use puppeteer

--- worker.js
+++ worker.js
@@ -1,1 +1,15 @@
 console.log('hi worker');
+
+const puppeteer = require('puppeteer');
+
+(async function () {
+    const browsers = await puppeteer.launch({
+        args: ["--no-sandbox",],
+        defaultViewport: { width: 850, height: 1920 }
+    });
+    const page = await browsers.newPage()
+
+    console.log(page);
+})();
+
+
